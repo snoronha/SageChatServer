9212d164a99504469e0e453efe3c9f98fd955285
Sanjay Noronha <sanjay@angel.co>
2017-09-15 23:25:15 -0700

    Added connection to Postgres

--- sage_chat.js
+++ sage_chat.js
@@ -1,6 +1,12 @@
 var app  = require('express')();
 var http = require('http').Server(app);
 var io   = require('socket.io')(http);
+var pg   = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://temba:temba@decrep.it:5432/temba';
+const dbClient = new pg.Client(connectionString);
+dbClient.connect();
+getStateDiagram(dbClient);
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -26,6 +32,13 @@ http.listen(3000, function(){
 
 
 //--------- UTILS ---------//
+function getStateDiagram(dbClient) {
+    dbClient.query("SELECT * FROM flows_ruleset", function(err, result) {
+        // console.log(result.rows.length);
+    });
+
+}
+
 function getCurrentTimestamp() {
     var now = new Date();
     // return now.toLocaleDateString("en-US") + " " + now.toLocaleTimeString("en-US");
